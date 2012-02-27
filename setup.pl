3f8b4317a579f4ccaa328fd3063496ab6d538792
Dee Newcum <dee.newcum@gmail.com>
2012-02-26 21:27:30 -0600

    initial commit

---
+++ setup.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+
+    use strict;
+    use warnings;
+
+    use Data::Dumper;
+    #use Devel::Comments;           # uncomment this during development to enable the ### debugging statements
+
+
+die "TODO: implement this";
+
+
+
