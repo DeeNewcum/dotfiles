83e685b7701b428d138b02bdc0db1582178f6023
Dee Newcum <dee.newcum@gmail.com>
2022-07-21 12:27:18 -0500

    new script -- build_gnu_screen.sh

---
+++ apps/ebuild/build_gnu_screen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# download source tarball from https://ftp.gnu.org/gnu/screen/
+#
+# known to work with GNU Screen versions -- 4.9.0, (TODO)
+
+set -x
+
+[ -e Makefile ] && make clean       # in case we're re-running this script
+
+./autogen.sh                                    || exit
+./configure --prefix=$HOME/apps/screen/         || exit
+./config.status                                 || exit
+make                                            || exit
+make install                                    || exit
+
+cd $HOME/apps/screen/;  find -type f -exec ls -l {} \;
